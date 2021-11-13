51cff16d8f9e2cec71a53eaa594a878cc06ab011
Vladimir Avchenov <vladimir.lsk.cool@gmail.com>
2021-11-13 07:11:23 +0300

    password auth, autoreconnect, config file

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const { Conn } = require('@rob9315/mcproxy');
 const mc = require('minecraft-protocol');
 const EventEmitter = require('events');
+require('dotenv').config();
 let client2b2t, proxy;
 
 class Client2b2t extends EventEmitter{
-    #username;
-    #password;
+    #options = {
+        username: 'username',
+        host: '2b2t.org',
+        port: 25565,
+        version: '1.12.2',
+        profilesFolder: './accounts'
+    };
     #server;
     #client;
-    #host = '2b2t.org';
-    #port = 25565;
     #positionInQueue;
     #inQueue;
 
     constructor(options){
         super();
-        let { username, password, host, port } = options;
-        this.#username = username;
-        this.#password = password;
-        this.#host = host || this.#host;
-        this.#port = port || this.#port;
+        this.#options = {...this.#options, ...options};
+    }
+
+    disconnect(){
+        if(this.#server){
+            this.#server.disconnect();
+            this.#server = null;
+        }
     }
 
     connect(){
-        this.#server = new Conn({
-            username: this.#username,
-            password: this.#password,
-            host: this.#host,
-            port: this.#port,
-            version: '1.12.2'
-        });
+        this.disconnect();
+        this.#server = new Conn(this.#options);
         this.#client = this.#server.bot._client;
         this.#inQueue = true;
 
@@ -39,7 +41,6 @@ class Client2b2t extends EventEmitter{
                     if (this.#inQueue) {
                         const headerMessage = JSON.parse(data.header);
                         try {
-                            console.log(headerMessage);
                             this.#positionInQueue = parseInt(headerMessage.text.split("\n")[5].substring(25));
                             this.#inQueue = true;
                             super.emit('queueAdvance', this.#positionInQueue);
@@ -58,6 +59,13 @@ class Client2b2t extends EventEmitter{
                     }
                     break;
             }
+        });
+
+        this.#client.on('end', () => {
+            super.emit('disconnect', arguments);
+        });
+        this.#client.on('error', () => {
+            super.emit('disconnect', arguments);
         });
     }
 
@@ -78,83 +86,114 @@ class Client2b2t extends EventEmitter{
     }
 }
 
-function start(){
-    client2b2t = new Client2b2t({
-        username: ``,
-        password: ``
+proxy = mc.createServer({
+    'online-mode': false,
+    encryption: true,
+    host: '0.0.0.0',
+    port: 25565,
+    version: '1.12.2',
+    'max-players': 1,
+    motd: 'Proxy started, but not connected to server!',
+});
+proxy.on('login', (client) => {
+    const server = (client2b2t) ? client2b2t.server : null;
+    try{
+        server.generatePackets()[0][1].entityId;
+    }catch (e) {
+        client.end("Proxy not connected to 2b2t!");
+        return;
+    }
+
+    // Spawn on "space" location
+    client.write('login', {
+        entityId: server.generatePackets()[0][1].entityId,
+        gameMode: 3,
+        dimension: 1,
+        difficulty: 0,
+        levelType: 'flat'
     });
-    client2b2t.connect();
+    client.write('position', { x: 0, y: 240, z: 0, yaw: 0, pitch: 0, flags: 0, teleportId: 1 });
+    client.write('chat', { message: JSON.stringify({ text: '§6§o§l4bored4wait§r\n' +
+                'Password required! Use §l/login §e<password>§r' }) });
 
-    proxy = mc.createServer({
-        'online-mode': false,
-        encryption: true,
-        host: '0.0.0.0',
-        port: 25565,
-        version: '1.12.2',
-        'max-players': 1
-    });
-    proxy.on('login', (client) => {
-        const server = client2b2t.server;
-        if(!server) {
-            client.end("Proxy not connected to 2b2t!");
-            return;
-        }
+    client.on("end", () => {
+        //startAntiAntiAFK();
+    })
 
-        // Spawn on "space" location
-        client.write('login', {
-            entityId: server.generatePackets()[0][1].entityId,
-            gameMode: 3,
-            dimension: 1,
-            difficulty: 0,
-            levelType: 'flat'
-        });
-        client.write('position', { x: 0, y: 240, z: 0, yaw: 0, pitch: 0, flags: 0, teleportId: 1 });
-        client.write('chat', { message: JSON.stringify({ text: '§6§o§l4bored4wait§r\n' +
-                    'Password required! Use §l/login §e<password>§r' }) });
-
-        client.on("end", () => {
-            //startAntiAntiAFK();
-        })
-
-        let linkedWith2b2t = false;
-        client.on("chat", (data) => {
-            if(!linkedWith2b2t && data && data.message[0] === '/'){
-                const userInput = data.message.slice(1).split(" ");
-                switch(userInput[0]){
-                    case 'login':
-                        if(!userInput[1]){
-                            client.write('chat', { message: JSON.stringify({ text: 'Use §l/login §e<password>§r' })});
+    let linkedWith2b2t = false;
+    client.on("chat", (data) => {
+        if(!linkedWith2b2t && data && data.message[0] === '/'){
+            const userInput = data.message.slice(1).split(" ");
+            switch(userInput[0]){
+                case 'login':
+                    if(!userInput[1]){
+                        client.write('chat', { message: JSON.stringify({ text: 'Use §l/login §e<password>§r' })});
+                    }else{
+                        if(client.username === process.env.PROXY_USERNAME && userInput[1] === process.env.PROXY_PASSWORD){
+                            // Send all packages expect first 'login' package
+                            if(!server) {
+                                client.end("Proxy not connected to 2b2t!");
+                                break;
+                            }
+                            linkedWith2b2t = true;
+                            for (let packet of server.generatePackets().slice(1)) {
+                                client.write(packet[0], packet[1]);
+                            }
+                            server.link(client);
                             break;
                         }else{
                             client.write('chat', { message: JSON.stringify({ text: '§cWrong password! Try again!§r' })});
-                            break;
                         }
-                        break;
-                    // TODO: Don't forget to remove!
-                    case 'testConnect':
-                        // Send all packages expect first 'login' package
-                        if(!server) {
-                            client.end("Proxy not connected to 2b2t!");
-                            break;
-                        }
-                        linkedWith2b2t = true;
-                        for (let packet of server.generatePackets().slice(1)) {
-                            client.write(packet[0], packet[1]);
-                        }
-                        server.link(client);
-                        break;
-                }
+                    }
+                    break;
             }
-        });
+        }
     });
+});
 
-    client2b2t.on('queueAdvance', function(pos){
-        proxy.motd = `§6Position in queue: §l${pos}§r\n§6Estimated time: §l${'Coming soon...'}§r`;
-    });
+function start2b2t(){
+    if(client2b2t){
+        client2b2t.disconnect();
+        client2b2t = null;
+    }
+    mc.ping({host: '2b2t.org', port: 25565}, (err) => {
+        if(err){
+            console.error('Ping not send to 2b2t!');
+            reconnectTimeouted();
+        }
+        else {
+            console.log('Yay! Successfully pinged to 2b2t!');
+            client2b2t = new Client2b2t({
+                username: process.env.MINECRAFT_USERNAME,
+                password: process.env.MINECRAFT_PASSWORD,
+                auth: process.env.MINECRAFT_TYPE
+            });
+            client2b2t.connect();
 
-    client2b2t.on('queueComplete', function(){
-        proxy.motd = `§6You can play now :)§r`;
+            client2b2t.on('queueAdvance', function(pos){
+                console.log(`Position in queue: ${pos}`);
+                proxy.motd = `§6Position in queue: §l${pos}§r\n§6Estimated time: §l${'Coming soon...'}§r`;
+            });
+
+            client2b2t.on('queueComplete', function(){
+                console.log('You can play now :)');
+                proxy.motd = `§6You can play now :)§r`;
+            });
+
+            client2b2t.on('disconnect', function(data){
+                console.error('DISCONNECTED!');
+                console.error(data);
+                proxy.motd = `§c§bDISCONNECTED!§r`;
+                reconnectTimeouted();
+            });
+        }
     });
 }
 
-start();
+let reconnectTimeout;
+function reconnectTimeouted(){
+    if(reconnectTimeout) clearTimeout(reconnectTimeout);
+    reconnectTimeout = setTimeout(start2b2t, 5000);
+}
+
+start2b2t();
--- readme.md
+++ readme.md
@@ -5,10 +5,10 @@ Based on (2bored2wait)[https://github.com/themoonisacheese/2bored2wait]
 I really do not like how 2bored2wait is implemented, so I decided to write my own. 
 
 To Do:
-- [ ] Password auth (Entering a password in the chat)
-- [ ] Auto-reconnect
+- [x] Password auth (Entering a password in the chat)
+- [x] Auto-reconnect
 - [ ] Anti-AFK
 - [ ] Calculate ETA
 - [ ] Telegram notification
-- [ ] Config file
+- [x] Config file
 - [ ] Readme
